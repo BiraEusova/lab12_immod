8a4fc80cf0214fae895afc1a309499ecad2f6fac
Ka3achka <ira.breusova@gmail.com>
2021-05-16 04:29:38 +0700

    lab 12 was done

---
+++ lab8_immod/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace lab8_immod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelSemifinal2 = new System.Windows.Forms.Label();
+            this.labelFinal = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelFirstStep1 = new System.Windows.Forms.Label();
+            this.labelSemifinal1 = new System.Windows.Forms.Label();
+            this.labelFirstStep4 = new System.Windows.Forms.Label();
+            this.labelFirstStep3 = new System.Windows.Forms.Label();
+            this.labelFirstStep2 = new System.Windows.Forms.Label();
+            this.labelWinner = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1st Game (1 vs 2)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(760, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "4th Game (7 vs 8)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(590, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3rd Game (5 vs 6)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "2nd Game (3 vs 4)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(671, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "2nd Game";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(334, 177);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "1st Game";
+            // 
+            // labelSemifinal2
+            // 
+            this.labelSemifinal2.AutoSize = true;
+            this.labelSemifinal2.Location = new System.Drawing.Point(671, 223);
+            this.labelSemifinal2.Name = "labelSemifinal2";
+            this.labelSemifinal2.Size = new System.Drawing.Size(0, 20);
+            this.labelSemifinal2.TabIndex = 10;
+            // 
+            // labelFinal
+            // 
+            this.labelFinal.AutoSize = true;
+            this.labelFinal.Location = new System.Drawing.Point(515, 325);
+            this.labelFinal.Name = "labelFinal";
+            this.labelFinal.Size = new System.Drawing.Size(0, 20);
+            this.labelFinal.TabIndex = 12;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(515, 289);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(43, 20);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Final";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(25, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 37);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "First step";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(26, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 37);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Semifinal";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Info;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(26, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 37);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Final";
+            // 
+            // labelFirstStep1
+            // 
+            this.labelFirstStep1.AutoSize = true;
+            this.labelFirstStep1.Location = new System.Drawing.Point(267, 89);
+            this.labelFirstStep1.Name = "labelFirstStep1";
+            this.labelFirstStep1.Size = new System.Drawing.Size(0, 20);
+            this.labelFirstStep1.TabIndex = 17;
+            // 
+            // labelSemifinal1
+            // 
+            this.labelSemifinal1.AutoSize = true;
+            this.labelSemifinal1.Location = new System.Drawing.Point(334, 223);
+            this.labelSemifinal1.Name = "labelSemifinal1";
+            this.labelSemifinal1.Size = new System.Drawing.Size(0, 20);
+            this.labelSemifinal1.TabIndex = 20;
+            // 
+            // labelFirstStep4
+            // 
+            this.labelFirstStep4.AutoSize = true;
+            this.labelFirstStep4.Location = new System.Drawing.Point(760, 89);
+            this.labelFirstStep4.Name = "labelFirstStep4";
+            this.labelFirstStep4.Size = new System.Drawing.Size(0, 20);
+            this.labelFirstStep4.TabIndex = 21;
+            // 
+            // labelFirstStep3
+            // 
+            this.labelFirstStep3.AutoSize = true;
+            this.labelFirstStep3.Location = new System.Drawing.Point(590, 89);
+            this.labelFirstStep3.Name = "labelFirstStep3";
+            this.labelFirstStep3.Size = new System.Drawing.Size(0, 20);
+            this.labelFirstStep3.TabIndex = 22;
+            // 
+            // labelFirstStep2
+            // 
+            this.labelFirstStep2.AutoSize = true;
+            this.labelFirstStep2.Location = new System.Drawing.Point(430, 89);
+            this.labelFirstStep2.Name = "labelFirstStep2";
+            this.labelFirstStep2.Size = new System.Drawing.Size(0, 20);
+            this.labelFirstStep2.TabIndex = 23;
+            // 
+            // labelWinner
+            // 
+            this.labelWinner.AutoSize = true;
+            this.labelWinner.Location = new System.Drawing.Point(163, 412);
+            this.labelWinner.Name = "labelWinner";
+            this.labelWinner.Size = new System.Drawing.Size(0, 20);
+            this.labelWinner.TabIndex = 24;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.SystemColors.Info;
+            this.label15.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(26, 395);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(119, 37);
+            this.label15.TabIndex = 25;
+            this.label15.Text = "Winner";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(434, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 69);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Get Result";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 546);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.labelWinner);
+            this.Controls.Add(this.labelFirstStep2);
+            this.Controls.Add(this.labelFirstStep3);
+            this.Controls.Add(this.labelFirstStep4);
+            this.Controls.Add(this.labelSemifinal1);
+            this.Controls.Add(this.labelFirstStep1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.labelFinal);
+            this.Controls.Add(this.labelSemifinal2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelSemifinal2;
+        private System.Windows.Forms.Label labelFinal;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelFirstStep1;
+        private System.Windows.Forms.Label labelSemifinal1;
+        private System.Windows.Forms.Label labelFirstStep4;
+        private System.Windows.Forms.Label labelFirstStep3;
+        private System.Windows.Forms.Label labelFirstStep2;
+        private System.Windows.Forms.Label labelWinner;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab8_immod/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8_immod
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private const double lambda = 100;
+
+        private string Team1 = "1";
+        private string Team2 = "2";
+        private string Team3 = "3";
+        private string Team4 = "4";
+        private string Team5 = "5";
+        private string Team6 = "6";
+        private string Team7 = "7";
+        private string Team8 = "8";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Poisson()
+        {
+            int m = -1;
+            double S = 0;
+            do
+            {
+                m += 1;
+                double alpha = random.NextDouble();
+                S += Math.Log10(alpha);
+
+            } while (S >= -lambda);
+
+            return m;
+        }
+
+        string PlayTwoTeams(string teamFirst, string teamSecond)
+        {
+            int first = Poisson();
+            int second = Poisson();
+
+            while (first == second)
+            {
+                first = Poisson();
+                second = Poisson();
+            }
+
+            if (first > second)
+                return teamFirst;
+            else
+                return teamSecond;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string winTeam1 = PlayTwoTeams(Team1, Team2);
+            labelFirstStep1.Text = winTeam1;
+            string winTeam2 = PlayTwoTeams(Team3, Team4);
+            labelFirstStep2.Text = winTeam2;
+            string winTeam3 = PlayTwoTeams(Team5, Team6);
+            labelFirstStep3.Text = winTeam3;
+            string winTeam4 = PlayTwoTeams(Team7, Team8);
+            labelFirstStep4.Text = winTeam4;
+            string winTeamF1 = PlayTwoTeams(winTeam1, winTeam2);
+            labelSemifinal1.Text = winTeamF1;
+            string winTeamF2 = PlayTwoTeams(winTeam3, winTeam4);
+            labelSemifinal2.Text = winTeamF2;
+            string Champion = PlayTwoTeams(winTeamF1, winTeamF2);
+            labelFinal.Text = Champion;
+            labelWinner.Text = Champion;
+        }
+    }
+}
